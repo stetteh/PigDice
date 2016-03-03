748f8ef1fc267b988d3f7ddec108881b8e100740
Seth Tetteh-Quaye <stettehq@gmail.com>
2016-03-02 22:06:14 -0600

    loop for more rolls

--- PigDice/Program.cs
+++ PigDice/Program.cs
@@ -11,23 +11,34 @@ namespace PigDice
         static Random rnd = new Random();
         static void Main(string[] args)
         {
+            int numOfRolls = 0;
+            int totalScore = 0;
+            
             //create title
             Console.WriteLine("Welcome to Pig Dice Game");
-            Console.WriteLine("Please rol your dice to start the game");
+            Console.WriteLine("Please roll your dice to start the game");
             Console.ReadLine();
 
             // Console.Clear();
+            bool keepPlaying = true;
 
-            int diceNum = rnd.Next(1, 7);
-            Console.WriteLine($"you have rolled {diceNum}");
-            Console.ReadLine();
-
-            if(diceNum == 1)
+            while (keepPlaying)
             {
-                Console.WriteLine("you lost");
-                break;
-            }
+                int diceNum = rnd.Next(1, 7);
+                Console.WriteLine($"you have rolled {diceNum}");
+                Console.ReadLine();
 
+                if (diceNum == 1)// if 1 is rolled the game stops 
+                {
+                    Console.WriteLine("you lost");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("press enter to keep rolling");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 
