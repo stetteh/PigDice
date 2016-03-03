3c65befe4ae0fda106c689befabb6290c8d0cb76
Seth Tetteh-Quaye <stettehq@gmail.com>
2016-03-02 21:15:12 -0600

    checking to see if user rolled 1

--- PigDice/Program.cs
+++ PigDice/Program.cs
@@ -8,8 +8,27 @@ namespace PigDice
 {
     class Program
     {
+        static Random rnd = new Random();
         static void Main(string[] args)
         {
+            //create title
+            Console.WriteLine("Welcome to Pig Dice Game");
+            Console.WriteLine("Please rol your dice to start the game");
+            Console.ReadLine();
+
+            // Console.Clear();
+
+            int diceNum = rnd.Next(1, 7);
+            Console.WriteLine($"you have rolled {diceNum}");
+            Console.ReadLine();
+
+            if(diceNum == 1)
+            {
+                Console.WriteLine("you lost");
+                break;
+            }
+
         }
     }
+
 }
